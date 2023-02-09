396751fbdb402b704e82d48a560c30ce35efffee
kjungw1025 <kjungw1025@naver.com>
2023-02-09 14:49:26 +0900

    4. 삭제 기능

--- Vue/my-todo1/src/components/TodoMain.vue
+++ Vue/my-todo1/src/components/TodoMain.vue
@@ -4,7 +4,7 @@
         <header><h1>Vue Fire todo1</h1></header>
         <main>
             <div class="todos">
-                <div class="write">
+                <div class="write" v-if="writeState === 'add'"> <!-- 등록 -->
                     <input
                         ref="writeArea" 
                         type="text"
@@ -13,7 +13,17 @@
                     />
                     <button class="btn add" v-on:click="addItem">Add</button>
                 </div>
-                <!-- 1. 할일 목록 리스팅 -->
+
+                <div class="write" v-else> <!-- 수정 -->
+                    <input
+                        ref="editArea" 
+                        type="text"
+                        v-model="editItemText"
+                        v-on:keyup.enter="editSave"
+                    />
+                    <button class="btn add" v-on:click="editSave">Save</button>
+                </div>
+
                 <ul class="list">
                     <li v-for="(todo, i) of todos" :key="todo.text">
                         <i 
@@ -23,8 +33,8 @@
                         <span>
                             {{ todo.text }}
                             <b>
-                                <a href="">Edit</a>
-                                <a href="">Del</a>
+                                <a href="" @click.prevent="editShow(i)">Edit</a>
+                                <a href="" @click.prevent="del(i)">Del</a>
                             </b>
                         </span>
                     </li>
@@ -38,7 +48,10 @@
 export default {
     data() {
         return {
+            writeState: 'add',
             addItemText: '',
+            crrEditItem: '',
+            editItemText: '',
             todos:[
                 {text: '공부하기', state: 'yet'},
                 {text: '운동하기', state: 'done'},
@@ -70,6 +83,19 @@ export default {
                 this.todos[i].state = 'yet';
             }
 
+        },
+        editShow(index) {
+            this.crrEditItem = index;
+            this.writeState = 'edit';
+            this.editItemText = this.todos[index].text;
+        },
+        editSave() {
+            this.todos[this.crrEditItem].text = this.editItemText;
+            this.editItemText = '';
+            this.writeState = 'add';
+        },
+        del(index) {
+            this.todos.splice(index, 1);
         },
     },
     mounted() {
